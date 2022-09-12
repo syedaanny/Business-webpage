e796a008c7092775170367eb7c547a251620766e
Syeda <syedaanny02@gmail.com>
2022-09-12 20:26:56 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+  }
+  
+  function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+  }
+
+
+  .left-average-title-border h3 {width: 19%;height: 0.11em;background: #d37e009e;margin-top: .19em;margin-right: .31em;}
